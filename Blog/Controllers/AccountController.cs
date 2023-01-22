45e31e0582c2463deddd497cf028188c14ddcc9c
Albert Souza <albert.souza@inovafarma.com.br>
2023-01-22 20:11:14 -0300

    improve: implements user registration endpoint

--- Blog/Controllers/AccountController.cs
+++ Blog/Controllers/AccountController.cs
@@ -1,10 +1,14 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog.Data;
+using Blog.Extensions;
+using Blog.Models;
 using Blog.Services;
+using Blog.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecureIdentity.Password;
 
 namespace Blog.Controllers
 {
@@ -16,6 +20,45 @@ namespace Blog.Controllers
         public AccountController(TokenService tokenService)
             => _tokenService = tokenService;
 
+        [HttpPost("v1/users/register")]
+        public async Task<IActionResult> CreateAccount([FromBody] RegisterViewModel model, [FromServices] BlogDataContext context)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
+
+            User user = new()
+            {
+                Name = model.Name,
+                Email = model.Email,
+                Slug = model.Email.Replace("@", "-").Replace(".", "-")
+            };
+
+            var password = PasswordGenerator.Generate(length: 25);
+            user.PasswordHash = PasswordHasher.Hash(password);
+
+            try
+            {
+                await context.Users.AddAsync(user);
+                await context.SaveChangesAsync();
+
+                return CreatedAtAction(actionName: nameof(GetUserName),
+                                           routeValues: new { id = user.Id },
+                                           value: new ResultViewModel<dynamic>(new
+                                           {
+                                               user = user.Email,
+                                               password
+                                           }));
+            }
+            catch (DbUpdateException)
+            {
+                return StatusCode(400, new ResultViewModel<string>(error: "This e-mail address is already used."));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<string>(error: "Internal error."));
+            }
+        }
+
         [HttpPost("v1/login")]
         public IActionResult Login()
         {
@@ -24,16 +67,21 @@ namespace Blog.Controllers
             return Ok(token);
         }
 
-        [Authorize(Roles = "user")]
-        [HttpGet("v1/user")]
-        public IActionResult GetUser() => Ok(User.Identity.Name);
+        [HttpGet("v1/users/{id}")]
+        public IActionResult GetUserName([FromServices] BlogDataContext context, [FromRoute] int id)
+        {
+            try
+            {
+                var user = context.Users.FirstOrDefault(x => x.Id == id);
+                if (user is null)
+                    return BadRequest(new ResultViewModel<User>(error: "User not found."));
 
-        [Authorize(Roles = "author")]
-        [HttpGet("v1/author")]
-        public IActionResult GetAuthor() => Ok(User.Identity.Name);
-
-        [Authorize(Roles = "admin")]
-        [HttpGet("v1/admin")]
-        public IActionResult GetAdmin() => Ok(User.Identity.Name);
+                return Ok(new ResultViewModel<string>(data: user.Name));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<User>(error: "Internal error."));
+            }
+        }
     }
 }
---
+++ Blog/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
